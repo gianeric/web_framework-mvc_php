4633ddb7c60621e00f5ea2cdfd6db1dc9629c592
Gian Eric <gianeric08@gmail.com>
2020-07-18 10:46:05 -0300

    18/07/2020 Log/Observer

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes();
 // put -> atualizar um dado
 // delete -> excluir um data
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 // Route::get('/areas', 'AreaController@index');
 // Route::get('/areas/create', 'AreaController@create');
 // Route::post('/areas/store', 'AreaController@store');
@@ -35,8 +35,21 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Route::resource('areas', 'AreaController')
 //     ->except('show');
 
-Route::resource('areas', 'AreaController')
+Route::group(['prefix' => '', 'as' => '', 'middleware' => ['auth', 'log']], function () {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::group(['prefix' => '', 'as' => '', 'middleware' => ['admin']], function () {
+        Route::resource('areas', 'AreaController')
+            ->except('show');
+
+        Route::resource('studies', 'StudyController')
+            ->except('show');
+    });
+});
+
+/*Route::resource('areas', 'AreaController')
     ->except('show');
 
 Route::resource('studies', 'StudyController')
-    ->except('show');
+    ->except('show');*/
